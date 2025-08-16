67e8cbeb6656e281105edceca9f24c5907862952
nurbekomar <omarnurbek13@gmail.com>
2025-08-16 09:36:37 +0500

    add 404

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, Django!")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Page Not Found</h1>")
