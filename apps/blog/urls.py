bd69d36726a7bdb4e43eaa271e3b80360c1cec4d
nurbekomar <omarnurbek13@gmail.com>
2025-08-20 21:48:01 +0500

    update blog model and template

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,4 +4,5 @@ from apps.blog import views
 urlpatterns = [
     path("", views.blog, name="blog"),
     path("post/<int:post_id>", views.show_post, name="post"),
+    path("cats/<int:cat_id>", views.show_category, name="category"),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,8 +6,19 @@ from .models import *
 # Create your views here.
 def blog(request):
     posts = Post.objects.all()
-    return render(request, "blog/blog.html", {"posts": posts})
+    cats = Category.objects.all()
+
+    date = {"posts": posts, "cats": cats}
+    return render(request, "blog/blog.html", context=date)
 
 
 def show_post(request, post_id):
     return HttpResponse(f"<h1>Пост но id {post_id}<h1/>")
+
+
+def show_category(request, cat_id):
+    posts = Post.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    
+    date = {"posts": posts, "cats": cats}
+    return render(request, "blog/blog.html", context=date)
